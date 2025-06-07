70150c8917881c95e7911cec7718e92082583281
nicantmac <135231021+nicantmac@users.noreply.github.com>
2025-06-07 14:09:55 -0400

    Update how-to-point.cpp

--- Language Practicing/Operating-in-C++/intermediate-concepts/pointers/how-to-point.cpp
+++ Language Practicing/Operating-in-C++/intermediate-concepts/pointers/how-to-point.cpp
@@ -9,37 +9,31 @@ using namespace std;
 *****/
 
 int main() {
+    // Let's start with accessing memory and deferencing variables
     int students = 100; 
     int *ptrStudents;
     ptrStudents = &students; // ptrStudents stores the address of students
 
     // Print the memory address stored in ptrStudents
-    cout << "Memory address of students (stored in ptrStudents): " << ptrStudents << endl;
+    cout << "Memory address of (ptrStudents): " << ptrStudents << endl;
 
-    // Print the value at that address (dereference)
+    // Now print the value and not the address for students, hint -> (dereference)
     cout << "Value pointed to by ptrStudents: " << *ptrStudents << endl;
 
-    // Change the value of students
-    students = 45;
-    cout << "After changing students to 45, value pointed to by ptrStudents: " << *ptrStudents << endl;
-
-    // Change the value through the pointer!
-    *ptrStudents = 77;
-    cout << "After changing *ptrStudents to 77, students is now: " << students << endl;
-
+    
     // Demonstrating pointer usage with strings
     string joeBiden = "1600 Pennsylvania Avenue NW, Washington, DC 20500";
-    string roBoT = "1234 Doxxed Ln, Bolbingbor, Saturn 33901";
-    string *pointBot = &roBoT; // Pointer storing the memory address of roBoT
+    string roBoT = "1234 Doxxed Ln, Bolingborski, Saturn 33901";
+    string *ptrBot = &roBoT; // Pointer storing the memory address of roBoT
 
     cout << "\nThe address for the Former President is " << joeBiden << endl;
-    cout << "The address for the Robot is " << *pointBot << endl; // Dereferencing pointer to get the value
+    cout << "The address for the Robot is " << *ptrBot << endl; // Dereferencing pointer to get the value
 
     // Print the memory address of roBoT
-    cout << "Memory address of roBoT (stored in pointBot): " << pointBot << endl;
+    cout << "Memory address of roBoT (stored in pointBot): " << ptrBot << endl;
 
     // Change string through the pointer
-    *pointBot = "9876 Cyberspace Blvd, Matrix City, VR 40404";
+    *ptrBot = "9876 Cyberspace Blvd, Matrix City, VR 40404";
     cout << "After changing *pointBot, new Robot address is: " << roBoT << endl;
 
     return 0;
