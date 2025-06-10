3203008944846adafefdcd63f36d8525a4f2371a
nicantmac <135231021+nicantmac@users.noreply.github.com>
2025-06-10 01:22:48 -0400

    Create data-type-methods.py

---
+++ Language Practicing/Operating-in-Python/beginner-topics/differ-data-types/data-type-methods.py
@@ -0,0 +1,43 @@
+# data-type-methods.py
+# Demonstrating methods of common Python data types
+
+# String methods
+s = "hello world"
+
+print("\nString methods:")
+print("upper():", s.upper())
+print("capitalize():", s.capitalize())
+print("replace():", s.replace("world", "Python"))
+print("split():", s.split())
+
+# List methods
+lst = [3, 1, 4, 1, 5]
+
+print("\nList methods:")
+lst.append(9)
+print("append(9):", lst)
+
+lst.sort()
+print("sort():", lst)
+
+lst.reverse()
+print("reverse():", lst)
+
+print("count(1):", lst.count(1))
+
+# Dictionary methods
+d = {"a": 1, "b": 2}
+
+print("\nDictionary methods:")
+print("keys():", d.keys())
+print("values():", d.values())
+print("items():", d.items())
+
+# Set methods
+s1 = {1, 2, 3}
+s2 = {3, 4, 5}
+
+print("\nSet methods:")
+print("union():", s1.union(s2))
+print("intersection():", s1.intersection(s2))
+print("difference():", s1.difference(s2))
