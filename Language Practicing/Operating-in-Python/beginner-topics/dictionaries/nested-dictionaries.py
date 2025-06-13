213f6cbf3080944544afd2c4f0ae55a6b3b31724
nicantmac <135231021+nicantmac@users.noreply.github.com>
2025-06-13 01:11:10 -0400

    Update nested-dictionaries.py

--- Language Practicing/Operating-in-Python/beginner-topics/dictionaries/nested-dictionaries.py
+++ Language Practicing/Operating-in-Python/beginner-topics/dictionaries/nested-dictionaries.py
@@ -1,6 +1,5 @@
-# dict_nested.py
-# Working with nested dictionaries
-
+# Here is practice with nested dictionaries
+# The dictionary below is what's going to be manipulated
 students = {
     "student1": {
         "name": "Alice",
@@ -14,12 +13,14 @@ students = {
     }
 }
 
-# Access nested values
-print("Student1 name:", students["student1"]["name"])
-print("Student2 major:", students["student2"]["major"])
+# Here is how to access nested values in a dictionary
+print("Student1 name:", students["student1"]["name"])  # Output: Student1 name: Alice
+print("Student2 major:", students["student2"]["major"])  # Output: Student2 major: Math
 
-# Loop through nested dict
+
+# How to loop through nested dictionary
+# student_id = student1 & student2, the student_info = the keys-values within student1 & student2
 for student_id, student_info in students.items():
-    print(f"\n{student_id}:")
+    print(f"\n{student_id}:")  # Outputs: student1 & student2
     for key, value in student_info.items():
-        print(f"  {key}: {value}")
+        print(f"  {key}: {value}")  # Outputs students keys-values 
