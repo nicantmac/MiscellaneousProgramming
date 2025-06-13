f03abc17585588891c69966a030883a7bb3d2f00
nicantmac <135231021+nicantmac@users.noreply.github.com>
2025-06-13 00:51:48 -0400

    Update looping-dictionaries.py

--- Language Practicing/Operating-in-Python/beginner-topics/dictionaries/looping-dictionaries.py
+++ Language Practicing/Operating-in-Python/beginner-topics/dictionaries/looping-dictionaries.py
@@ -1,17 +1,17 @@
 # Here is a simple dictionary where we will manipulate it in different ways by looping
 my_dict = {"name": "Eve", "age": 22, "city": "Chicago"}
 
-# Loop through keys
+# Using the 'in' keyword, here we simply loop through the keys
 print("\nKeys:")
 for key in my_dict:
-    print(key)
+    print(key) 
 
-# Loop through values
+# To loop through the values, use the .values() method to retrieve the values
 print("\nValues:")
 for value in my_dict.values():
     print(value)
 
-# Loop through key-value pairs
+# To retrieve both keys-values, loop through the key-value pairs using the .items() method
 print("\nKey-Value pairs:")
 for key, value in my_dict.items():
     print(f"{key}: {value}")
