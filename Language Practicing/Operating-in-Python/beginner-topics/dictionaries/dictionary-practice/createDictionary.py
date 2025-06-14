75542dc24322fbebe500ceb36b9c9d46c94a90a5
nicantmac <135231021+nicantmac@users.noreply.github.com>
2025-06-14 17:07:20 -0400

    Update createDictionary.py

--- Language Practicing/Operating-in-Python/beginner-topics/dictionaries/dictionary-practice/createDictionary.py
+++ Language Practicing/Operating-in-Python/beginner-topics/dictionaries/dictionary-practice/createDictionary.py
@@ -1,19 +1,30 @@
-def create_dictionary(keys, values):
-    new_dict = {}  # Start with an empty dictionary
+'''
+PROMPT: Write a function create_dictionary() that takes in a list of keys and a list of values as parameters. 
+The function returns a dictionary where each item in keys is paired with a corresponding item in values.
 
-    for i in range(len(keys)):  # Loop over the indices of the keys/values lists
-        key = keys[i]           # Get the key at the current index
-        value = values[i]       # Get the corresponding value at the same index
-        new_dict[key] = value   # Add the key-value pair to the dictionary
+Expected Output: {"peanut": "butter", "dragon": "fly", "star": "fish", "pop": "corn", "space": "ship"}
+'''
 
-    return new_dict             # Return the completed dictionary
+# Here our function will be able to take both list; keys & values
+def create_dictionary_manual(keys, values):
+    new_dict = {}  # let's create a empty dictionary we want filled
+    
+    for i in range(len(keys)):  # lets 'for-loop' over the indices of the keys list
+        key = keys[i]  # get the current index in keys list
+        value = values[i]  # get the current index in values list
+        new_dict[key] = value  # Using dict[key] = val format, this will add the key-value pairs to the dictionary
+
+    return new_dict  # when all done, return the completed dictionary
 
 
+# More efficient method
+def create_dictionary_zip(keys, values):
+    new_dict = zip(keys, values)  # .zip() creates tuple pairs i.e. -> ('peanut', 'dragon'), ('star', 'pop')...
+    return dict(new_dict)  # dict() creates a dictionary of the two values respectively
 
-def create_dictionary(keys, values):
-    return dict(zip(keys, values))
-
-keys = ["peanut", "dragon", "star", "pop", "space"]
-values = ["butter", "fly", "fish", "corn", "ship"]
-
-print(create_dictionary(keys, values))
+if __name__ == '__main__':
+    keys = ["peanut", "dragon", "star", "pop", "space"]
+    values = ["butter", "fly", "fish", "corn", "ship"]
+    
+    print(create_dictionary_manual(keys, values))
+    print(create_dictionary_zip(keys, values))
