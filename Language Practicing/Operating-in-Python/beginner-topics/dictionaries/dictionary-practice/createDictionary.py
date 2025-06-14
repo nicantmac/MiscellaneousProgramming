dc9c6ef16c269e8424c85de10721f335c0730abb
nicantmac <135231021+nicantmac@users.noreply.github.com>
2025-06-14 17:42:49 -0400

    Update createDictionary.py

--- Language Practicing/Operating-in-Python/beginner-topics/dictionaries/dictionary-practice/createDictionary.py
+++ Language Practicing/Operating-in-Python/beginner-topics/dictionaries/dictionary-practice/createDictionary.py
@@ -5,22 +5,22 @@ The function returns a dictionary where each item in keys is paired with a corre
 Expected Output: {"peanut": "butter", "dragon": "fly", "star": "fish", "pop": "corn", "space": "ship"}
 '''
 
-# Here our function will be able to take both list; keys & values
+# METHOD 1: Here our function will be able to take both list; keys & values
 def create_dictionary_manual(keys, values):
-    new_dict = {}  # let's create a empty dictionary we want filled
+    new_dict = {}  # let's create a empty dictionary to get filled
     
     for i in range(len(keys)):  # lets 'for-loop' over the indices of the keys list
-        key = keys[i]  # get the current index in keys list
-        value = values[i]  # get the current index in values list
-        new_dict[key] = value  # Using dict[key] = val format, this will add the key-value pairs to the dictionary
+        key = keys[i]           # get the current index in keys list
+        value = values[i]       # get the current index in values list
+        new_dict[key] = value   # Using dict[key] = val format, this will add the key-value pairs to the dictionary
 
     return new_dict  # when all done, return the completed dictionary
 
 
-# More efficient method
+# METHOD 2: More efficient method USING .zip() & .dict()
 def create_dictionary_zip(keys, values):
     new_dict = zip(keys, values)  # .zip() creates tuple pairs i.e. -> ('peanut', 'dragon'), ('star', 'pop')...
-    return dict(new_dict)  # dict() creates a dictionary of the two values respectively
+    return dict(new_dict)         # dict() creates a dictionary of the two values respectively
 
 if __name__ == '__main__':
     keys = ["peanut", "dragon", "star", "pop", "space"]
