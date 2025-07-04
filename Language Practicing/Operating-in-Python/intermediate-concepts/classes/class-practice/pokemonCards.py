8c4ebbe439994c70cc24ff576e3f0f4415dccc2a
nicantmac <135231021+nicantmac@users.noreply.github.com>
2025-07-04 00:56:42 -0400

    Update pokemonCards.py

--- Language Practicing/Operating-in-Python/intermediate-concepts/classes/class-practice/pokemonCards.py
+++ Language Practicing/Operating-in-Python/intermediate-concepts/classes/class-practice/pokemonCards.py
@@ -3,11 +3,17 @@ class Pokemon:
     self.name = name
     self.rank = rank
 
-  def pokemonName(self):
+  def pName(self):
     print(f"My name is {self.name}")
 
-  def pokemonRank(self):
+  def pRank(self):
     print(f"My rank is {self.rank}")
 
 pokemon1 = Pokemon("Charizard", "Ultra")
 pokemon2 = Pokemon("Bulbasaur", "Master")
+
+pokemon1.pName()
+pokemon1.pRank()
+
+pokemon2.pName()
+pokemon2.pRank()
