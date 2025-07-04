a0aa6b0f26775091599c57e7cfd124177ec9dbdc
nicantmac <135231021+nicantmac@users.noreply.github.com>
2025-07-04 01:20:37 -0400

    Update clashRoyale.py

--- Language Practicing/Operating-in-Python/intermediate-concepts/classes/class-practice/clashRoyale.py
+++ Language Practicing/Operating-in-Python/intermediate-concepts/classes/class-practice/clashRoyale.py
@@ -1,1 +1,26 @@
-gfdbfd
+class Card:
+    def __init__(self, name, elixir_cost, rarity, card_type):
+      self.name = name
+      self.elixir_cost = elixir_cost
+      self.rarity = rarity
+      self.card_type = card_type
+
+    def display_info(self):
+      print(f"Name: {self.name}")
+      print(f"Cost: {self.elixir_cost} Elixir")
+      print(f"Rarity: {self.rarity}")
+      print(f"Type: {self.card_type}")
+
+    def isExpensive(self):
+      return self.elixir_cost >= 5
+
+  def isLegendary(self):
+      if self.rarity == "Legendary":
+        return f"This is a legendary card: {self.name} 🔥"
+      return f"{self.name} is not a legendary."
+        
+
+card1 = Card("Royal Giant", 6, "Common", "Troop")
+card1.display_info()
+print(card1.is_expensive())
+print(card1.isLegendary())
