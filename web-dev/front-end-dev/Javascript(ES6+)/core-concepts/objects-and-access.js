b2b54d41011977c0abcdebaadd8347260767b3eb
nicantmac <135231021+nicantmac@users.noreply.github.com>
2025-07-05 22:12:23 -0400

    Update objects-and-access.js

--- web-dev/front-end-dev/Javascript(ES6+)/core-concepts/objects-and-access.js
+++ web-dev/front-end-dev/Javascript(ES6+)/core-concepts/objects-and-access.js
@@ -1,1 +1,18 @@
-cvbnm
+const card = {
+  name: "Musketeer",
+  elixir: 4,
+  type: "Troop"
+};
+
+// Access properties
+console.log(card.name);  // "Musketeer"
+console.log(card["elixir"]); // 4
+
+// Add new property
+card.range = "Long";
+console.log(card);
+
+// Loop keys
+for (let key in card) {
+  console.log(key + ":", card[key]);
+}
