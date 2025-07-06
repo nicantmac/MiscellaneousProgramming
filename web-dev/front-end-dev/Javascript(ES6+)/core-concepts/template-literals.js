188e60fce4b72bb10e90b0db5c9aa093f05a1e36
nicantmac <135231021+nicantmac@users.noreply.github.com>
2025-07-05 22:14:44 -0400

    Update template-literals.js

--- web-dev/front-end-dev/Javascript(ES6+)/core-concepts/template-literals.js
+++ web-dev/front-end-dev/Javascript(ES6+)/core-concepts/template-literals.js
@@ -1,1 +1,8 @@
-dfghjkl
+let name = "P.E.K.K.A";
+let elixir = 7;
+
+// Traditional way
+console.log("Card: " + name + ", Cost: " + elixir);
+
+// Template literal way
+console.log(`Card: ${name}, Cost: ${elixir}`); // cleaner and readable
