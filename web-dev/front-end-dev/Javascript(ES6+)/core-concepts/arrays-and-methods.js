020820669acf00c06b2f06225a83e3a1381ebe51
nicantmac <135231021+nicantmac@users.noreply.github.com>
2025-07-05 22:11:04 -0400

    Update arrays-and-methods.js

--- web-dev/front-end-dev/Javascript(ES6+)/core-concepts/arrays-and-methods.js
+++ web-dev/front-end-dev/Javascript(ES6+)/core-concepts/arrays-and-methods.js
@@ -1,1 +1,15 @@
-fghjk
+let cards = ["Knight", "Archer", "Giant"];
+
+// Access
+console.log(cards[0]); // "Knight"
+
+// Add
+cards.push("Wizard");
+console.log(cards);
+
+// Remove
+cards.pop();
+console.log(cards);
+
+// Loop
+cards.forEach(card => console.log("Card:", card));
