0fae7ddbb8f7c47757f2768295072183f61d6fa2
nicantmac <135231021+nicantmac@users.noreply.github.com>
2025-07-05 22:10:08 -0400

    Update conditionals-loops.js

--- web-dev/front-end-dev/Javascript(ES6+)/core-concepts/conditionals-loops.js
+++ web-dev/front-end-dev/Javascript(ES6+)/core-concepts/conditionals-loops.js
@@ -1,1 +1,20 @@
-kjhg
+// If-else condition
+let elixir = 4;
+
+if (elixir >= 5) {
+  console.log("Expensive card");
+} else {
+  console.log("Playable card");
+}
+
+// Loop example
+for (let i = 1; i <= 3; i++) {
+  console.log("Battle " + i);
+}
+
+// While loop
+let count = 0;
+while (count < 2) {
+  console.log("Charge!");
+  count++;
+}
