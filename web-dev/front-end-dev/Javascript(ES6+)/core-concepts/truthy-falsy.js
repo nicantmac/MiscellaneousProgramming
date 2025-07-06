d0b79efd9368d1cc3262d88159d608c0c095977e
nicantmac <135231021+nicantmac@users.noreply.github.com>
2025-07-05 22:06:59 -0400

    Update truthy-falsy.js

--- web-dev/front-end-dev/Javascript(ES6+)/core-concepts/truthy-falsy.js
+++ web-dev/front-end-dev/Javascript(ES6+)/core-concepts/truthy-falsy.js
@@ -1,1 +1,14 @@
-fghjk
+// Falsy values: false, 0, "", null, undefined, NaN
+let val = 0;
+
+if (val) {
+  console.log("Truthy!");
+} else {
+  console.log("Falsy!"); // This will run
+}
+
+// Truthy values: anything not falsy
+let name = "Knight";
+if (name) {
+  console.log("Name is truthy"); // This will run
+}
