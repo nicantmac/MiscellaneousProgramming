3e02251e5c1b0be9f624b49160ebedf4e37fab77
nicantmac <135231021+nicantmac@users.noreply.github.com>
2025-07-05 22:16:46 -0400

    Update destructuring-spread.js

--- web-dev/front-end-dev/Javascript(ES6+)/core-concepts/destructuring-spread.js
+++ web-dev/front-end-dev/Javascript(ES6+)/core-concepts/destructuring-spread.js
@@ -1,1 +1,9 @@
-gjfdjl
+// Destructuring
+const card = { name: "Mega Minion", cost: 3 };
+const { name, cost } = card;
+console.log(name); // "Mega Minion"
+
+// Spread with arrays
+let deck1 = ["Archer", "Knight"];
+let deck2 = ["Giant", ...deck1]; // spreads deck1 into deck2
+console.log(deck2); // ["Giant", "Archer", "Knight"]
