7cb76e16c28311eeed0829eebf227d4d70cdaa3d
nicantmac <135231021+nicantmac@users.noreply.github.com>
2025-07-08 01:03:35 -0400

    Update comparison-operators.js

--- web-dev/front-end-dev/Javascript(ES6+)/core-concepts/comparison-operators.js
+++ web-dev/front-end-dev/Javascript(ES6+)/core-concepts/comparison-operators.js
@@ -1,14 +1,36 @@
-let a = 5;
-let b = "5";
+// Here we will understand how JavaScript does comparisons
 
-// Loose equality (==): compares values after type coercion
-console.log(a == b); // true
+let a = 5; // Initiate a number
+let b = "5"; // Initiate a string
 
-// Strict equality (===): compares values AND types
+/* - (==) => loose equality: compares values stored after type coercion
+   - In JavaScript, string holds hierarchy, type coercion converts the number 5 into a string to match "5"  */
+console.log(a == b); // Type Coercion: "5" == "5", Output: true
+
+/* - (===) => strict equality: compares values stored AND data types
+   - In JavaScript, strict comparison is three characters length (i.e. checks if boolean and true)  */
 console.log(a === b); // false
 
-// Other comparisons
+
+// Practice comparisons
+
+/* JavaScript converts the string a = "5" converts to the number 5, then compares both values
+   After type coercion, the equation becomes: 5 != 5, results = false  */
 console.log(a != b);  // false
+
+/* Strict equality compares the string a = "5" with number b = 5
+   The equation becomes: 5 !== "5", results = true  */
 console.log(a !== b); // true
+
+/* Simply checks if a = 5 greater than 3
+   The equation is: 5 > 3, results = true  */
 console.log(a > 3);   // true
+
+/* - JavaScript converts the string y = "7" into a number 7
+   - <, >, <=, and >= are a numeric operators so it becomes: 7 > 5  */
+let y = "7";
+console.log(y > 5); // true
+
+/* Simply checks if a = 5 less than or equal to 5
+   The equation is: 5 <= 5, results = true  */
 console.log(a <= 5);  // true
