5a268f56a4392525814f2fb5239e8945ddc01ed2
nicantmac <135231021+nicantmac@users.noreply.github.com>
2025-07-10 09:13:07 -0400

    Update clashRoyale.py

--- Language Practicing/Operating-in-Python/intermediate-concepts/classes/class-practice/clashRoyale.py
+++ Language Practicing/Operating-in-Python/intermediate-concepts/classes/class-practice/clashRoyale.py
@@ -5,22 +5,27 @@ class Card:
       self.rarity = rarity
       self.card_type = card_type
 
+    # Create method to display attributes of the card
     def display_info(self):
       print(f"Name: {self.name}")
       print(f"Cost: {self.elixir_cost} Elixir")
       print(f"Rarity: {self.rarity}")
       print(f"Type: {self.card_type}")
 
+    # Create method to check if the card is expensive
     def isExpensive(self):
       return self.elixir_cost >= 5
 
-  def isLegendary(self):
+    # Create method to check if the card is a legendary ranked card
+    def isLegendary(self):
       if self.rarity == "Legendary":
         return f"This is a legendary card: {self.name} 🔥"
       return f"{self.name} is not a legendary."
         
-
+# We're creating: the Royal Giant, costing 6 elixir, ranked common level, and is a ground troop
 card1 = Card("Royal Giant", 6, "Common", "Troop")
+
+# Next, try calling display_info, is_expensive, and is_legendary methods 
 card1.display_info()
-print(card1.is_expensive())
+print(card1.is_expensive()) 
 print(card1.isLegendary())
