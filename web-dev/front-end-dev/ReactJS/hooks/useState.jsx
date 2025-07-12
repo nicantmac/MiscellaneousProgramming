ade8a37dc4d87f1d35282b8034066ba383ee93ee
nicantmac <135231021+nicantmac@users.noreply.github.com>
2025-07-12 00:36:56 -0400

    Update useState.jsx

--- web-dev/front-end-dev/ReactJS/hooks/useState.jsx
+++ web-dev/front-end-dev/ReactJS/hooks/useState.jsx
@@ -1,1 +1,18 @@
-fghjk
+import React, { useState } from 'react';
+
+function modifyElement() {
+  const [count, setCount] = useState(0)
+
+  function() { (count) => setCount(count + 1); }
+  
+  return(
+    <div className="page-container">
+      <div>
+        <h1>Current Counter</h1>
+        <button onClick={countUp}>Click Me!</button>
+        <p>`The current count: ${count}`</p>
+      </div>
+    </div>
+  );
+}
+export default modifyElement;
