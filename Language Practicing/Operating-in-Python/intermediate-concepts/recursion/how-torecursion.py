98059d4cb0a84d2cfc963bde8e0a9c47f0d60053
nicantmac <135231021+nicantmac@users.noreply.github.com>
2025-07-21 16:12:20 -0400

    Create how-torecursion.py

---
+++ Language Practicing/Operating-in-Python/intermediate-concepts/recursion/how-torecursion.py
@@ -0,0 +1,36 @@
+# def repeat_hello_1(n):
+#     if n > 0:
+#         print("Hello")
+#         repeat_hello_1(n - 1)
+# repeat_hello_1(5)
+
+# def repeat_hello_2(num):
+#     while num > 0:
+#         print(f"Hello - {num}")
+#         num -= 1
+#
+# repeat_hello_2(5)
+
+# def factorial(num):
+#     if num == 0:
+#         return 1
+#     else:
+#         return num * factorial(num - 1)
+#
+# print(factorial(5))
+
+# def sum_list(lst):
+#  if not lst: return 0
+#  return lst[0] + sum_list(lst[1:])
+#
+# theList = [1, 2, 3, 4, 5]
+# print(sum_list(theList))
+
+# def is_power_of_two(num):
+#   if num == 1: return True
+#   if num % 2 == 1: return False
+#
+#   return is_power_of_two(num//2)
+#
+# print(is_power_of_two(32))
+
