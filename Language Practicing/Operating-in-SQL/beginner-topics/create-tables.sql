d3a85f8f2f4ebf4de3957744702ffbe7f763522c
Nick Mack <135231021+nicantmac@users.noreply.github.com>
2025-11-02 23:56:41 -0500

    Create employed_DB and Employee_TABLE
    Created a new database and an Employee table with relevant columns.

--- Language Practicing/Operating-in-SQL/beginner-topics/create-tables.sql
+++ Language Practicing/Operating-in-SQL/beginner-topics/create-tables.sql
@@ -1,1 +1,14 @@
-fdf
+-- Create db and use the db to run the sql data
+CREATE DATABASE employed_DB;
+USE employed_DB;
+
+-- First to create tables utilize the keywords; CREATE TABLE
+-- After creating your table, add the columns you want
+CREATE TABLE  Employee_TABLE (
+    Employee_ID INT PRIMARY KEY,
+    FirstName VARCHAR(30) NOT NULL,
+    LastName VARCHAR(30) NOT NULL,
+    Department VARCHAR(35),
+    Employee_Salary DECIMAL(10, 2) DEFAULT 35000.00,
+    HireDate DATE
+);
