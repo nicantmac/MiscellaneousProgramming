6520d8180ebad839eef3a09544221fbd98550e7b
Nick Mack <135231021+nicantmac@users.noreply.github.com>
2025-12-29 14:57:48 -0500

    Enhance ToggleBoolean component with comments
    Added comments to explain state management and button behavior.

--- web-dev/front-end-dev/ReactJS/hooks/useState/toggleSwitch.jsx
+++ web-dev/front-end-dev/ReactJS/hooks/useState/toggleSwitch.jsx
@@ -1,11 +1,14 @@
 import { useState } from "react";
 
+// component to display the btn state
 export default function ToggleBoolean() {
+ // isOn stores state, setIsOn is used to change the state, useState keyword used to initiate data
   const [isOn, setIsOn] = useState(false);
 
   return (
+    // when btn clicked, setIsOn changes to opposite state
     <button onClick={() => setIsOn(prev => !prev)}>
-      {isOn ? "ON" : "OFF"}
+      {isOn ? "ON" : "OFF"} {/* text is based isOn, if true 'ON' if not 'OFF' */}
     </button>
   );
 }
