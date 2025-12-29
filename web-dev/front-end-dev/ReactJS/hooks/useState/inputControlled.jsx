1e6ca43f8f575c4bbd50f208579f44f3a6776392
Nick Mack <135231021+nicantmac@users.noreply.github.com>
2025-12-29 15:11:50 -0500

    Enhance comments in InputControlled component
    Added comments to clarify the purpose of the component and its functionality.

--- web-dev/front-end-dev/ReactJS/hooks/useState/inputControlled.jsx
+++ web-dev/front-end-dev/ReactJS/hooks/useState/inputControlled.jsx
@@ -1,13 +1,18 @@
 import { useState } from "react";
 
+// component will display data
 export default function InputControlled() {
+  // value stores data, setValue will modify data, useState initialized with empty str
   const [value, setValue] = useState("");
 
   return (
+    // input will be used to change state of whats typed
     <input
-      value={value}
-      onChange={e => setValue(e.target.value)}
-      placeholder="Type here..."
+      value={value} {/* the input will always show exactly whats stored in the state */}
+      placeholder="Type here..." {/* shows "Type here..." when the input is empty */}
+
+      {/* onchange is an event listener that triggers every time the user types. It updates the state with the current string typed */}
+      onChange={(e) => setValue(e.target.value)}
     />
   );
 }
