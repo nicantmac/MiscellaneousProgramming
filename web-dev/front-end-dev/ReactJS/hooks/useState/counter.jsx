180d21481e74df58623bc069a830ba1a7deb87c1
Nick Mack <135231021+nicantmac@users.noreply.github.com>
2025-12-29 14:39:58 -0500

    Create counter.jsx

---
+++ web-dev/front-end-dev/ReactJS/hooks/useState/counter.jsx
@@ -0,0 +1,13 @@
+import { useState } from "react";
+
+export default function BasicCounter() {
+  const [count, setCount] = useState(0);
+
+  return (
+    <div>
+      <p>Count: {count}</p>
+      <button onClick={() => setCount(c => c + 1)}>+</button>
+      <button onClick={() => setCount(c => c - 1)}>-</button>
+    </div>
+  );
+}
