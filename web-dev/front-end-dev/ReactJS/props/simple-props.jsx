21a2f57bfed2d7b5195aa65093879b4e47bb3b49
Nick Mack <135231021+nicantmac@users.noreply.github.com>
2025-12-28 22:20:35 -0500

    Update simple-props.jsx

--- web-dev/front-end-dev/ReactJS/props/simple-props.jsx
+++ web-dev/front-end-dev/ReactJS/props/simple-props.jsx
@@ -1,1 +1,23 @@
-vfds
+// child component
+/* writing "props" now holds all data stated from parent.
+   use dot notation to access values, i.e. props.name -> Jordan */
+function WelcomeMessage(props) {
+  return (
+    <div style={{ border: "1px solid #ccc", padding: "10px" }}>
+      {/* dot notation let's us access specific data */}
+      <h1>Hello, {props.name}!</h1>
+      <p>You have {props.messageCount} new notifications.</p>
+    </div>
+  );
+}
+
+// parent component
+export default function App() {
+  return (
+    <div>
+      {/* created child component to show user name & message count */}
+      <WelcomeMessage name="Jordan" messageCount={5} />
+      <WelcomeMessage name="Taylor" messageCount={0} />
+    </div>
+  );
+}
