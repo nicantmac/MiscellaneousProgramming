a9d2b4ac7db4c731822b56976690a0bc8555bcb7
Nick Mack <135231021+nicantmac@users.noreply.github.com>
2025-12-29 00:33:10 -0500

    Update how-to-useState.jsx

--- web-dev/front-end-dev/ReactJS/hooks/useState/how-to-useState.jsx
+++ web-dev/front-end-dev/ReactJS/hooks/useState/how-to-useState.jsx
@@ -1,18 +1,26 @@
+// import the 'useState' hook from the react library to manage local component state
 import { useState } from 'react';
 
 function modifyElement() {
-  const [count, setCount] = useState(0)
+  /* format: 'count' -> stores the state, 
+             'setCount' -> is used to modify the state,
+             'useState' -> sets the initial value stored */
+  const [count, setCount] = useState(0);
 
-  function() { (count) => setCount(count + 1); }
+  const countUp = () => { 
+    // use the modifier 'setCount' to increment the state by 1
+    /* note: its good practice to use previous state and render the next desired state when 
+       updating so React doesn't mess up focusing on the actual live count, incase user clicks too fast */
+    setCount(prevCount => prevCount + 1); 
+  };
   
   return(
-    <div className="page-container">
-      <div>
-        <h1>Current Counter</h1>
-        <button onClick={countUp}>Click Me!</button>
-        <p>`The current count: ${count}`</p>
-      </div>
-    </div>
+    <main className="page-container">
+      <h1>Current Counter</h1>
+      <button onClick={countUp}>Click Me!</button>  {/* countUp function will execute onClick event */}
+      <p>{`The current count: ${count}`}</p>  {/* to track we use a template literal to display state */}
+    </main>
   );
 }
+
 export default modifyElement;
