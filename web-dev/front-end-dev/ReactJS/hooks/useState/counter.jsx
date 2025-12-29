357f4e18b567222b490c253f706c92aab14778b3
Nick Mack <135231021+nicantmac@users.noreply.github.com>
2025-12-29 14:51:17 -0500

    Enhance counter component with explanatory comments
    Added comments to explain the counter functionality.

--- web-dev/front-end-dev/ReactJS/hooks/useState/counter.jsx
+++ web-dev/front-end-dev/ReactJS/hooks/useState/counter.jsx
@@ -1,11 +1,15 @@
 import { useState } from "react";
 
+// component to display button count
 export default function BasicCounter() {
+  // count holds the data, setCount will change data, useState keyword will initiate the value
   const [count, setCount] = useState(0);
 
   return (
     <div>
-      <p>Count: {count}</p>
+      <p>Count: {count}</p> {/* here display count */}
+
+      {/* both btns use 'setCount' as a function to update count */}
       <button onClick={() => setCount(c => c + 1)}>+</button>
       <button onClick={() => setCount(c => c - 1)}>-</button>
     </div>
