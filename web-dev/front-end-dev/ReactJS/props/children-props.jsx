8ce73d6a16e870ea761c7515389f64fc3eda4761
Nick Mack <135231021+nicantmac@users.noreply.github.com>
2025-12-28 22:44:24 -0500

    Add Card component to handle children props

---
+++ web-dev/front-end-dev/ReactJS/props/children-props.jsx
@@ -0,0 +1,19 @@
+// child component
+function Card({ children }) {
+  return (
+    <div className="rounded-xl border shadow-lg p-6 bg-white">
+      {children} {/* This is the slot where the Parent's h1 and p tags go */}
+    </div>
+  );
+}
+
+// parent component
+export default function App() {
+  return (
+    <Card>
+      {/* Everything here becomes the "children" */}
+      <h1>Welcome to my Page</h1>
+      <p>This text is sitting inside the Card's shell.</p>
+    </Card>
+  );
+}
