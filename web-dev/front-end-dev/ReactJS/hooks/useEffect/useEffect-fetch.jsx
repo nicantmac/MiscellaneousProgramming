695b3ba8915b659351ca6d50dfc196b18d5a422d
Nick Mack <135231021+nicantmac@users.noreply.github.com>
2025-12-30 01:44:29 -0500

    Improve data fetching with error handling
    Refactor fetchData function to include error handling and update fetch URL.

--- web-dev/front-end-dev/ReactJS/hooks/useEffect/useEffect-fetch.jsx
+++ web-dev/front-end-dev/ReactJS/hooks/useEffect/useEffect-fetch.jsx
@@ -1,21 +1,41 @@
+
 import { useEffect, useState } from "react";
 
+// component that will display user fetched data
 export default function FetchExample() {
+  // data: data from user will be stored, setData: data retrieves data, useState initializes to null
   const [data, setData] = useState(null);
+  // loading stores loading state, setLoading will change loading state, useState initializes at true
   const [loading, setLoading] = useState(true);
 
+  // useEffect handles fetching user data on first mounts
   useEffect(() => {
-    async function fetchData() {
-      const res = await fetch("https://jsonplaceholder.typicode.com/posts/1");
-      const json = await res.json();
-      setData(json);
-      setLoading(false);
-    }
-
+    /* define an async function handles operations that take time, 
+       such as making network requests (i.e. fetching data from an API) */
+    const fetchData = async () => {
+      try {
+        // res: use fetch() for raw response from server, await pauses execution until the request completes
+        const res = await fetch("jsonplaceholder.typicode.com");
+        
+        // json: using .json() parses data & converts the raw response into a usable JavaScript object
+        const json = await res.json();
+        
+        setData(json); // setData: update the state with the user data
+      } catch (error) {
+         // catch handles failures to retrieve user data
+        console.error("Caught error:", error);
+      } finally {
+        setLoading(false); // finally: ALWAYS end loading whether success or fail
+      }
+    };
+            
+    // call that function to execute
     fetchData();
-  }, []);
+  }, []); // Empty dependency array ensures this only runs once on mount
 
+  // 'loading...' renders when state is true 
   if (loading) return <p>Loading...</p>;
 
+  // JSON.stringify converts a JavaScript object into a string
   return <pre>{JSON.stringify(data, null, 2)}</pre>;
 }
