eedab54a0baeee5b6f1a748d191938d1a6294e7b
Nick Mack <135231021+nicantmac@users.noreply.github.com>
2025-12-29 23:13:11 -0500

    Update useEffect-dependency.jsx

--- web-dev/front-end-dev/ReactJS/hooks/useEffect/useEffect-dependency.jsx
+++ web-dev/front-end-dev/ReactJS/hooks/useEffect/useEffect-dependency.jsx
@@ -1,15 +1,20 @@
+// import the 'useEffect' & 'useState' hook from the react library to manage the local component state
 import { useEffect, useState } from "react";
 
+// component to display data
 export default function UseEffectDependency() {
+  // count stores count, setCount will change count data, useState initializes count at 0
   const [count, setCount] = useState(0);
 
+  // useEffect will mount off first render and always update if count data ever changes
   useEffect(() => {
     console.log("Count changed:", count);
   }, [count]);
 
   return (
-    <button onClick={() => setCount(c => c + 1)}>
-      Count: {count}
+    // based onClick, the setCount will add 1 to count state
+    <button onClick={() => setCount(prevCount => prevCount + 1)}>
+      Count: {count} {/* here will display count data */}
     </button>
   );
 }
