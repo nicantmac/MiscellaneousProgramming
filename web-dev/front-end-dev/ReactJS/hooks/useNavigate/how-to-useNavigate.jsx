b0c8c4b62446599663d7afefec34e93b6c6bc6ff
Nick Mack <135231021+nicantmac@users.noreply.github.com>
2025-12-31 12:51:11 -0500

    Update how-to-useNavigate.jsx

--- web-dev/front-end-dev/ReactJS/hooks/useNavigate/how-to-useNavigate.jsx
+++ web-dev/front-end-dev/ReactJS/hooks/useNavigate/how-to-useNavigate.jsx
@@ -1,1 +1,22 @@
-gfhtfdg
+// import useNavigate hook to programmatically change routes inside your application
+import { useNavigate } from "react-router-dom";
+
+// component to display data
+function Login() {
+  /* navigate stores an imperative function (specifically a NavigateFunction). When you call useNavigate(), it returns 
+     this function, which allows you to change the browser's URL and navigate programmatically within your code */
+  const navigate = useNavigate();
+
+  function handleSubmit(e) {
+    e.preventDefault(); // prevent the browser from 
+    // login logic here
+    navigate("/dashboard");
+  }
+
+  return (
+    // onSubmit we invoke handleSubmit
+    <form onSubmit={handleSubmit}>
+      <button type="submit">Login</button>
+    </form>
+  );
+}
