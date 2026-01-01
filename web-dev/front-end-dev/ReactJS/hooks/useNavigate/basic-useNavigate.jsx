2b49b463a2565a89c62806d1db60a73af329f74a
Nick Mack <135231021+nicantmac@users.noreply.github.com>
2026-01-01 15:39:32 -0500

    Update basic-useNavigate.jsx

--- web-dev/front-end-dev/ReactJS/hooks/useNavigate/basic-useNavigate.jsx
+++ web-dev/front-end-dev/ReactJS/hooks/useNavigate/basic-useNavigate.jsx
@@ -1,9 +1,13 @@
+// 
 import { useNavigate } from "react-router-dom";
 
+// component for displaying data
 function Home() {
+  // to use navigate hook, store the function into variable
   const navigate = useNavigate();
 
   return (
+    // onClick -> btn invokes function that calls navigate hook to dashboard
     <button onClick={() => navigate("/dashboard")}>
       Go to Dashboard
     </button>
