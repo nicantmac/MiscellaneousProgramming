42a5131a1cae2cc66e544aa909a67b5f211f2594
Nick Mack <135231021+nicantmac@users.noreply.github.com>
2026-01-02 02:13:54 -0500

    Create counter.jsx

---
+++ web-dev/front-end-dev/ReactJS/hooks/useRef/counter.jsx
@@ -0,0 +1,21 @@
+/* quick note: useRef() returns an object that looks like this -> { current: initialValue } 
+   use dot notation (i.e. myRef.current) to access and modify this value  */
+
+import { useRef, useEffect } from 'react';
+
+// component displays data
+function Counter() {
+  // clickCount: stores cureent value, useRef() initializes value at 0
+  const clickCount = useRef(0);
+
+  // handleClick invokes onclick & adds 1 to current value
+  const handleClick = () => {
+    clickCount.current += 1; // access & add 1 to current value
+    console.log('Button clicked', clickCount.current, 'times'); // log current value
+  };
+
+  return( 
+    <h1>Button Counter Page</h1>
+    <button onClick={handleClick}>Click Me (check console)</button>
+  );
+}
