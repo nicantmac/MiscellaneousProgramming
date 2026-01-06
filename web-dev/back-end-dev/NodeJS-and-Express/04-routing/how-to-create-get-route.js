b272db5767fcb03e7baacebb8d5ae042905c9cb0
Nick Mack <135231021+nicantmac@users.noreply.github.com>
2026-01-05 20:48:39 -0500

    Update how-to-create-get-route.js

--- web-dev/back-end-dev/NodeJS-and-Express/04-routing/how-to-create-get-route.js
+++ web-dev/back-end-dev/NodeJS-and-Express/04-routing/how-to-create-get-route.js
@@ -1,1 +1,24 @@
-vbsdfu
+/**
+ * HOW TO CREATE A GET ROUTE IN EXPRESS
+ *
+ * A GET route responds when the browser or client
+ * makes a GET request to a specific URL.
+ */
+
+const express = require("express");
+const app = express();
+
+/**
+ * This route runs ONLY when someone visits:
+ * http://localhost:3000/hello
+ */
+app.get("/hello", (req, res) => {
+  res.send("Hello from Express");
+});
+
+/**
+ * Start the server
+ */
+app.listen(3000, () => {
+  console.log("Server running on http://localhost:3000");
+});
