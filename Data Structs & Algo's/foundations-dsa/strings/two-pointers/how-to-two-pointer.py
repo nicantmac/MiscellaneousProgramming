cdd75fa26be6c211f319f90bc5775414cf5565ee
Nick Mack <135231021+nicantmac@users.noreply.github.com>
2026-01-30 17:18:02 -0500

    Update how-to-two-pointer.py

--- Data Structs & Algo's/foundations-dsa/strings/two-pointers/how-to-two-pointer.py
+++ Data Structs & Algo's/foundations-dsa/strings/two-pointers/how-to-two-pointer.py
@@ -1,1 +1,19 @@
-fghjk
+What goes in how_to_two_pointers.py?
+
+The generic template
+
+Typical pointer moves:
+
+move both inward
+
+move only left
+
+move only right
+
+Common mistakes:
+
+off-by-one
+
+forgetting to update pointers
+
+mixing index vs value
