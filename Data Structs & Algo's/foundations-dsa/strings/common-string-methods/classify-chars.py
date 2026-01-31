cc34a1b47d25f55237a4cc12cfd3ee428260ed14
Nick Mack <135231021+nicantmac@users.noreply.github.com>
2026-01-31 11:50:38 -0500

    Update classify-chars.py

--- Data Structs & Algo's/foundations-dsa/strings/common-string-methods/classify-chars.py
+++ Data Structs & Algo's/foundations-dsa/strings/common-string-methods/classify-chars.py
@@ -1,1 +1,14 @@
-gmregpotr
+Used for filtering, validation, counting
+
+s.isalpha()      # letters only
+s.isdigit()      # digits only
+s.isalnum()      # letters + digits
+s.isspace()      # spaces / tabs / newlines
+s.islower()      # all lowercase
+s.isupper()      # all uppercase
+
+🔥 Most tested:
+
+isalpha()
+isdigit()
+isalnum()
