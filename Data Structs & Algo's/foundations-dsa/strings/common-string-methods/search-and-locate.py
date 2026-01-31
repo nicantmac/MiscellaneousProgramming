95f2aa40ece2889c06c3309abb8320d9ae256549
Nick Mack <135231021+nicantmac@users.noreply.github.com>
2026-01-31 11:52:07 -0500

    Update search-and-locate.py

--- Data Structs & Algo's/foundations-dsa/strings/common-string-methods/search-and-locate.py
+++ Data Structs & Algo's/foundations-dsa/strings/common-string-methods/search-and-locate.py
@@ -1,1 +1,19 @@
-jo
+Used for substring problems & validation
+
+s.find(sub)        # index or -1
+s.index(sub)       # index or error
+s.count(sub)       # frequency
+s.startswith(sub)
+s.endswith(sub)
+
+🔥 Most tested:
+
+find()
+
+count()
+
+startswith()
+
+endswith()
+
+⚠️ find() > index() for safety in interviews.
