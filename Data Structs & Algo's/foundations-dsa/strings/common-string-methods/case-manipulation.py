5e1f783e546712f83d66016cb83cfbacff7a0027
Nick Mack <135231021+nicantmac@users.noreply.github.com>
2026-01-31 11:49:42 -0500

    Update case-manipulation.py

--- Data Structs & Algo's/foundations-dsa/strings/common-string-methods/case-manipulation.py
+++ Data Structs & Algo's/foundations-dsa/strings/common-string-methods/case-manipulation.py
@@ -1,1 +1,5 @@
-vlfmsb
+s.lower()        # convert to lowercase
+s.upper()        # convert to uppercase
+s.title()        # Title Case (rare, but good to know)
+s.capitalize()   # Capitalize first character
+s.swapcase()     # swap upper <-> lower
